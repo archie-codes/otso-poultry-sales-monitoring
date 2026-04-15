c588ed5a4c3c9ed9598b939675320f15f95bf783
archie-codes <bauzonarchie@gmail.com>
2026-04-15 14:41:55 +0800

    Put bg slate 100

--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -77,7 +77,7 @@ export default function AppLayout({
   }
 
   return (
-    <div className="flex h-screen overflow-hidden bg-background">
+    <div className="flex h-screen overflow-hidden bg-slate-100 dark:bg-background">
       {/* ---> NEW: Pass the state and toggle function to the Sidebar <--- */}
       <Sidebar
         role={role}
